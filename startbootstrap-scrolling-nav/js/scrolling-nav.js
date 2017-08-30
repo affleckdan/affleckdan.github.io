54d849b9eb2a9c82b5e0fa313a9b27b194f97380
Dan Affleck <affleckdan@gmail.com>
2017-08-30 14:21:57 -0400

    edits

--- startbootstrap-scrolling-nav/js/scrolling-nav.js
+++ startbootstrap-scrolling-nav/js/scrolling-nav.js
@@ -27,3 +27,13 @@
   });
 
 })(jQuery); // End of use strict
+
+
+// Bouncing arrow bottom of full screen
+// Different arrow choices from Font Awesome
+// Fades away as you scroll down
+ $(window).scroll(function(){
+    $(".arrow").css("opacity", 1 - $(window).scrollTop() / 250);
+  //250 is fade pixels
+  });
+
